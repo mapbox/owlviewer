b4c42f3abd3d47164d10232c505565c0672fc716
Alex Barth <alex@developmentseed.org>
2012-10-30 19:46:23 -0400

    Track location.

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ $(function() {
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '<a href="http://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'
     }).addTo(map);
+    if (location.hash && location.hash.indexOf('#') === 0) {
+        var e = location.hash.substring(1).split('/');
+        if (e.length == 3) {
+            map.setView(
+                new L.LatLng(parseFloat(e[0]), parseFloat(e[1])),
+                parseFloat(e[2]));
+        }
+    }
+
+    map.on('moveend', function() {
+        var c = map.getCenter();
+        location.hash = c.lat + '/' + c.lng + '/' + map.getZoom();
+    });
 });
