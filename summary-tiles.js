f0f6119548bf526ec933ab3ddd59a352dae801d1
Paweł Paprota <ppawel@fastmail.fm>
2012-11-03 21:43:16 +0100

    Use LayerGroup to manage markers.

--- summary-tiles.js
+++ summary-tiles.js
@@ -1,13 +1,27 @@
 // Load tiled JSON summary and display markers.
 // Requires jQuery for jsonp.
 L.TileLayer.OWLSummaryTiles = L.TileLayer.extend({
+
+    initialize: function (url, options) {
+        this._url = url;
+        this._markers = L.layerGroup();
+        L.Util.setOptions(this, options);
+    },
+
+    onAdd: function (map) {
+        L.TileLayer.prototype.onAdd.call(this, map);
+        this._markers.addTo(map);
+    },
+
     data: null,
+
     _addTile: function(tilePoint, container) {
         if (this._getZoomForUrl() > 10) {
             return;
         }
         this._loadTile(null, tilePoint);
     },
+
     _loadTile: function (tile, tilePoint) {
         var layer = this;
         $.ajax({
@@ -16,9 +30,7 @@ L.TileLayer.OWLSummaryTiles = L.TileLayer.extend({
             success: function(json) {
                 var point = layer._map.layerPointToLatLng(layer._getTilePos(tilePoint));
                 var summaryIcon = L.divIcon({html: '<h3>' + json.num_changesets + '</h3>'});
-
-                //tilePoint.x * layer._map.getPixelBounds().min.x / layer.options.tileSize;
-                L.marker(point, {icon: summaryIcon}).addTo(layer._map);
+                layer._markers.addLayer(L.marker(point, {icon: summaryIcon}));
                 layer._tileLoaded();
             },
             error: function() {
@@ -26,12 +38,14 @@ L.TileLayer.OWLSummaryTiles = L.TileLayer.extend({
             }
         });
     },
+
     _resetCallback: function() {
         this.data = null;
         L.TileLayer.prototype._resetCallback.apply(this, arguments);
     },
-  _update: function() {
+
+    _update: function() {
         this.data = null;
-      L.TileLayer.prototype._update.apply(this, arguments);
-  }
+        L.TileLayer.prototype._update.apply(this, arguments);
+    }
 });
