6943d31694300c00c888eb453bbbadadfe037510
Alex Barth <alex@developmentseed.org>
2012-11-05 13:30:39 -0500

    Fix #16 dupe requests made for tiles
    - Cancel requests on _reset
    - Reset features hash on new data coming in
    - Assemble features array lazily
    - Track tiles in this._tiles - this will avoid loading tiles that have already been loaded.

--- geojson-tiles.js
+++ geojson-tiles.js
@@ -1,36 +1,49 @@
 // Load tiled GeoJSON and merge into single geojson hash.
 // Requires jQuery for jsonp.
 L.TileLayer.GeoJSON = L.TileLayer.extend({
-    data: null,
+    _geojson: {"type":"FeatureCollection","features":[]},
+    _requests: [],
+    geoJSON: function() {
+        if (this._geojson.features.length) return this._geojson;
+        for (k in this._tiles) {
+            var t = this._tiles[k];
+            if (t.geojson && t.geojson.features) {
+                this._geojson.features =
+                    this._geojson.features.concat(t.geojson.features);
+            }
+        }
+        return this._geojson;
+    },
     _addTile: function(tilePoint, container) {
-        this._loadTile(null, tilePoint);
+        var tile = { geojson: null };
+        this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
+        this._loadTile(tile, tilePoint);
     },
     _loadTile: function (tile, tilePoint) {
         var layer = this;
-        $.ajax({
+        this._requests.push($.ajax({
             url: this.getTileUrl(tilePoint),
             dataType: 'jsonp',
             success: function(geojson) {
-                if (!layer.data) {
-                    layer.data = geojson;
-                } else if (geojson.features) {
-                    layer.data.features = layer.data.features || [];
-                    layer.data.features =
-                        layer.data.features.concat(geojson.features);
-                }
+                tile.geojson = geojson;
+                layer._geojson.features = [];
                 layer._tileLoaded();
             },
             error: function() {
                 layer._tileLoaded();
             }
-        });
+        }));
     },
     _resetCallback: function() {
-        this.data = null;
+        this._geojson.features = [];
         L.TileLayer.prototype._resetCallback.apply(this, arguments);
+        for (i in this._requests) {
+            this._requests[i].abort();
+        }
+        this._requests = [];
     },
-	_update: function() {
-        this.data = null;
-	    L.TileLayer.prototype._update.apply(this, arguments);
-	}
+    _update: function() {
+        if (this._map._panTransition && this._map._panTransition._inProgress) { return; }
+        L.TileLayer.prototype._update.apply(this, arguments);
+    }
 });
