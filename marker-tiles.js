f39289af5668478c42ed768ba40c78c8480c549c
Alex Barth <alex@developmentseed.org>
2012-11-08 00:01:50 -0500

    Remove gunk.

--- marker-tiles.js
+++ marker-tiles.js
@@ -1,7 +1,6 @@
 // Load data and track location of tiles.
 L.TileLayer.Marker = L.TileLayer.Data.extend({
     _addTile: function(tilePoint, container) {
-        console.log(tilePoint, this._getTilePos(tilePoint), this._getTilePos(tilePoint).subtract({x: 125, y: 125}));
         var tile = {
             data: null,
             location: this._map.layerPointToLatLng(this._getTilePos(tilePoint).add({x: 125, y: 125}))
