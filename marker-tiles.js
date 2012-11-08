530650b66fa7cb7200c0accb6bb677cd7c54679f
Alex Barth <alex@developmentseed.org>
2012-11-08 00:02:21 -0500

    Better formatting.

--- marker-tiles.js
+++ marker-tiles.js
@@ -3,7 +3,9 @@ L.TileLayer.Marker = L.TileLayer.Data.extend({
     _addTile: function(tilePoint, container) {
         var tile = {
             data: null,
-            location: this._map.layerPointToLatLng(this._getTilePos(tilePoint).add({x: 125, y: 125}))
+            location: this._map.layerPointToLatLng(
+                this._getTilePos(tilePoint).add({x: 125, y: 125})
+            )
         };
         this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
         this._loadTile(tile, tilePoint);
